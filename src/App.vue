993f882c2e4a989122d3e7c7210c86cd4768ad27
mdjalalsk <mdjalalsk02@gmail.com>
2025-04-14 18:28:38 +0600

    project init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+
+   <h1 class="text-red-500">hello how are you?</h1>
+   <div class="flex flex-col items-center justify-center">
+    <p>Hello World</p>
+    <p> Hello Test</p>
+    <p> Hello Vue</p>
+   </div>
+</template>
+
+<style scoped>
+
+</style>
